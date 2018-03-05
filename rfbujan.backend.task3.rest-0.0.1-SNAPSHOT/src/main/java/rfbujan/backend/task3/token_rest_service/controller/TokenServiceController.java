fcb7790d41d39c81bc51057a510a034133cf7c50
Fernández Buján <ramon.fernandez.bujan@cgi.com>
2018-03-05 17:58:20 +0100

    first commit for the task3 of the backend exercise
    Rest API to offer the functionality of the SimpleAsyncTokenService implemented in the previous task.

---
+++ rfbujan.backend.task3.rest-0.0.1-SNAPSHOT/src/main/java/rfbujan/backend/task3/token_rest_service/controller/TokenServiceController.java
@@ -0,0 +1,26 @@
+package rfbujan.backend.task3.token_rest_service.controller;
+
+import java.util.concurrent.CompletableFuture;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import rfbujan.backend.task2.athenticator.TokenAuthenticatorRandomDelay;
+import rfbujan.backend.task2.common.model.Credentials;
+import rfbujan.backend.task2.common.model.UserToken;
+import rfbujan.backend.task2.common.token.provider.TokenProvider;
+import rfbujan.backend.task2.token_creator.TokenCreatorWithRandomDelay;
+import rfbujan.backend.task2.token_provider.SimpleAsyncTokenService;
+
+@RestController
+public class TokenServiceController {
+
+	private final TokenProvider simpleAsyncTokenProvider = new SimpleAsyncTokenService(new TokenCreatorWithRandomDelay(), new TokenAuthenticatorRandomDelay());
+	
+	@RequestMapping("/token")
+	public CompletableFuture<UserToken> requestToken(@RequestParam(value="username") String name, @RequestParam(value="password") String password)
+	{
+		return simpleAsyncTokenProvider.requestTokenAsync(new Credentials(name, password));
+	}
+}
